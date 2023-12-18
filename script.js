ed6a9737ab7913c3343eac3ecd0ff62b0f163d64
diksha6163 <79846515+diksha6163@users.noreply.github.com>
2023-12-19 00:02:44 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('generateBtn').addEventListener('click', function() {
+    var codeOutput = document.getElementById('codeOutput');
+    var motivationalCode = `console.log('Hello, world! Keep going and you will achieve great things!');`;
+    codeOutput.textContent = motivationalCode;
+});
